851bdfcef298650a55b8f71b5463b350b37c7ba1
olahandroid <80737684+olahandroid@users.noreply.github.com>
2021-03-17 11:33:01 +0700

    Update colors.ts
    asd

--- src/theme/colors.ts
+++ src/theme/colors.ts
@@ -1,17 +1,17 @@
 import { Colors } from "./types";
 
 export const baseColors = {
-  failure: "#8a6a10",
-  primary: "#8a6a10",
-  primaryBright: "#8a6a10",
-  primaryDark: "#8a6a10",
-  secondary: "#8a6a10",
-  success: "#8a6a10",
-  warning: "#FFB237",
+  failure: "#ff6f60",
+  primary: "#b0bec5",
+  primaryBright: "#cfd8dc",
+  primaryDark: "#90a4ae",
+  secondary: "#9f9f9f",
+  success: "#76d275",
+  warning: "#5c007a",
 };
 
 export const brandColors = {
-  binance: "#8a6a10",
+  binance: "#F0B90B",
 };
 
 export const lightColors: Colors = {
@@ -21,33 +21,33 @@ export const lightColors: Colors = {
   backgroundDisabled: "#E9EAEB",
   contrast: "#191326",
   invertedContrast: "#FFFFFF",
-  input: "#FC6400",
-  tertiary: "#FC6400",
-  text: "#8a6a10",
-  textDisabled: "#8a6a10",
-  textSubtle: "#8a6a10",
-  borderColor: "#8a6a10",
+  input: "#EFF4F5",
+  tertiary: "#EFF4F5",
+  text: "#2c2c2c",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#6d6d6d",
+  borderColor: "#E9EAEB",
   card: "#FFFFFF",
   gradients: {
-    bubblegum: "linear-gradient(139.73deg, #8a6a10 0%, #F3EFFF 100%)",
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
   },
 };
 
 export const darkColors: Colors = {
   ...baseColors,
   ...brandColors,
-  secondary: "#9A6AFF",
-  background: "#343135",
+  secondary: "#e2e9f0",
+  background: "#100C18",
   backgroundDisabled: "#3c3742",
   contrast: "#FFFFFF",
   invertedContrast: "#191326",
   input: "#483f5a",
   primaryDark: "#0098A1",
   tertiary: "#353547",
-  text: "#FC6400",
-  textDisabled: "#FC6400",
-  textSubtle: "#FC6400",
-  borderColor: "#FC6400",
+  text: "#F2F6F9",
+  textDisabled: "#9b95a7",
+  textSubtle: "#e2e9f0",
+  borderColor: "#524B63",
   card: "#27262c",
   gradients: {
     bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
